22209dfca252cf1c925c07ae948ea2c48105a59a
faria abid <faria.ju.2k16@gmail.com>
2021-04-15 05:46:40 +0600

    appointment added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Link
 } from "react-router-dom";
 import Home from "./Components/Home/Home/Home";
+import Appointment from "./Components/Appointment/Appointment/Appointment";
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home></Home>
+        </Route>
+        <Route path="/appointment">
+          <Appointment></Appointment>
         </Route>
       </Switch>
     </Router>
